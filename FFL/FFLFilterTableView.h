9ebb01a808ff4144b3ab84ddb934accd8370a359
imjonos <imnosok@gmail.com>
2014-08-05 02:38:48 +0400

    add filter

---
+++ FFL/FFLFilterTableView.h
@@ -0,0 +1,13 @@
+//
+//  FFLFilterTableView.h
+//  FFL
+//
+//  Created by NOs on 04.08.14.
+//  Copyright (c) 2014 NOs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FFLFilterTableView : UITableView
+
+@end
---
+++ FFL/FFLFilterTableView.m
@@ -0,0 +1,31 @@
+//
+//  FFLFilterTableView.m
+//  FFL
+//
+//  Created by NOs on 04.08.14.
+//  Copyright (c) 2014 NOs. All rights reserved.
+//
+
+#import "FFLFilterTableView.h"
+
+@implementation FFLFilterTableView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
